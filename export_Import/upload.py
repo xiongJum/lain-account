05beec8abff54fd2492e91debdaa0924896b5704
Lain <xiongjum@outlook.com>
2023-04-09 14:58:32 +0800

    修复导入文件时,时间格式没有格式化的问题

--- export_Import/upload.py
+++ export_Import/upload.py
@@ -1,4 +1,4 @@
-import pandas, json, time
+import pandas, json, datetime
 from config import Common
 
 
@@ -23,8 +23,12 @@ class Upload():
         row = pf.last_valid_index()
         if not row: exit('请添加文件后进行导入')
         # 将时间或则日期格式化为字符串
-        # if 'date' in pf: pf['date'] = pf['date'].dt.strftime('%Y-%m-%d')
-        # if 'date_created' in pf:  pf['date_created'] = pf['date_created'].dt.strftime('%Y-%m-%d %H:%M:%S')
+        if 'date' in pf:
+                if type(pf['date']) != str:
+                    pf['date'] = pf['date'].dt.strftime('%Y-%m-%d')
+        if 'date_created' in pf:
+                if pf['date_created'].empty:
+                    pf['date_created'] = pf['date_created'].dt.strftime('%Y-%m-%d %H:%M:%S')
         import_data = [list() for i in range(row+1)]
         # 将读取的数据,从json转换为列表
         read_csv = json.loads(pf.to_json())
